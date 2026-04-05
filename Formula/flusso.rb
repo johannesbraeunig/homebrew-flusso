924437844debbcf4202401c90cfbd59c2a9eb99f
Johannes Bräunig <johannes.braeunig@gmail.com>
2026-04-05 19:08:10 +0200

    Fix SHA256 checksum for v0.0.1

--- Formula/flusso.rb
+++ Formula/flusso.rb
@@ -2,7 +2,7 @@ class Flusso < Formula
   desc "Sync MoneyMoney transactions to YNAB"
   homepage "https://github.com/johannesbraeunig/flusso"
   url "https://github.com/johannesbraeunig/flusso/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "f88bf40cc1f5634b5bd68b339d83124b7a7976ee2357df7c4117b29ca6f045fa"
   license "MIT"
 
   depends_on :macos
